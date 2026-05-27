c639039c5860aaecd1fd9f9e2953d42ed711eb18
Yink <yink.info@gmail.com>
2026-05-27 13:30:46 +0200

    update

--- StudentGradeAnalyzer.py
+++ StudentGradeAnalyzer.py
@@ -5,7 +5,7 @@ import json
 def menu():
     while True:
         try:
-            choice = int(input("1. show grades\n2. add grades\n3. average grade\n4. highest/lowest grade\n5. failed/passed amount\n6. full statistics\n7 quit\n Enter: "))
+            choice = int(input("1. show grades\n2. add grades\n3. average grade\n4. highest/lowest grade\n5. failed/passed amount\n6. full statistics\n7. Delete grades\n8. Quit\n Enter: "))
             if choice == 1:
                 show_grades()
             if choice == 2:
@@ -19,6 +19,8 @@ def menu():
             if choice == 6:
                 statistics()
             if choice == 7:
+                delete_grades()
+            if choice == 8:
                 break
         except ValueError:
             print("Not a proper number")
@@ -28,7 +30,8 @@ def show_grades():
     grades = np.array(grades_normal)
 
     if len(grades) > 0:
-        print(grades)
+        for index, grade in enumerate(grades_normal(), start=1):
+            print(f"{index}. {grade}")
     else:
         print("No grades to print.")
 
@@ -58,7 +61,6 @@ def add_grades():
         except:
             return
         
-    
 
 def load_grades():
     try:
@@ -70,6 +72,36 @@ def load_grades():
         grades_normal = []
         grades = np.array(grades_normal)
         return grades
+
+def delete_grades():
+    grades = load_grades()
+    show_grades()
+    try:
+        delete = int(input("Which grade would you like to delete? (Type the number): ") - 1)
+        grade_deleted = grades[delete]
+        grades = np.delete(grades, delete)
+        grades_normal = grades.tolist()
+        with open("grades.json", "w") as f:
+            json.dump(grades_normal, f)
+        print(f"{grade_deleted} succesfully deleted")
+    except:
+        print("Invalid entry.")
+
+def failed_passed():
+    grades = load_grades()
+    failed = grades[grades < 5]
+    print(f"Failed grades: {len(failed)}")
+    print(f"Passed grades: {len(grades) - len(failed)}")
+
+def statistics():
+    grades = load_grades()
+    failed = grades[grades < 5]
+    print(f"Amount of grades: {len(grades)}\nAverage grade: {np.mean(grades)}\nHighest grade: {np.max(grades)}\nLowest grade: {np.min(grades)}\nMedian grade: {np.median(grades)}\nStandard Derivation: {np.std(grades)}\nFailed grades: {len(failed)}\nPassed grades: {len(grades)-len(failed)}\nSorted: {np.sort(grades)}")
+
+
+
+
+
 
 if __name__ == "__main__":
     menu()
